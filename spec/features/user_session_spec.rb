ab3ab9dfb7bb7e91702ecbdf6e218eb1f3e112e8
James Okunlade <james.okunlade@gmail.com>
2020-02-06 12:46:06 +0100

    signs in a valid user check added to user_session_spec

--- spec/features/user_session_spec.rb
+++ spec/features/user_session_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'User authentication', type: :feature do
-    it 'does not sign in unknown user' do
+  it 'signs in a valid user' do
+        User.create!(name: 'Test Example', email: 'test@example.com', password: 'okay123')
         visit new_user_session_path
-        fill_in 'Email', with: 'unknown@example.com'
-        fill_in 'Password', with: 'unknownpassword'
+        fill_in 'Email', with: 'test@example.com'
+        fill_in 'Password', with: 'okay123'
         click_button 'Log in'
-        expect(current_path).to eql(new_user_session_path)
-        expect(page).to have_content 'Sign in'
-    end
+        expect(current_path).to eql(root_path)
+        expect(page).to have_content('Sign out')
+  end
+  
+  it 'does not sign in unknown user' do
+      visit new_user_session_path
+      fill_in 'Email', with: 'unknown@example.com'
+      fill_in 'Password', with: 'unknownpassword'
+      click_button 'Log in'
+      expect(current_path).to eql(new_user_session_path)
+      expect(page).to have_content 'Sign in'
+  end
 end
